9a85de2c2b588345317fb5394a686ff4ddb9f433
Cooper Nichols <coopnich92@gmail.com>
2020-01-16 13:22:58 -0600

    almost done

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 const header1Content = document.querySelector(".article__header");
-header1Content.textContent = "Welcome to Cooper's blog!"
+header1Content.textContent = "Welcome to Cooper's blog!";
+
+// const headerNodes = document.querySelectorAll(".article__header");
+// for (let i = 0; i < headerNodes.length; i++) {
+//     headerNodes.classList.remove("article__header")
+//     headerNodes.classList.add("article__header important")
+// };
+
+
+const dashedRemoval = document.querySelector(".dashed")
+dashedRemoval.classList.remove("dashed")
+
+const footerColor = document.querySelector(".article__footer")
+footerColor.classList.add("goldenrod")
+
